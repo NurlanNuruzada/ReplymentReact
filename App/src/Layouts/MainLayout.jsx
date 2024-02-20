612488cc0090710fb648ab69d17c9d77ac53d5c3
nurlannuruzada <nurlangn@code.edu.az>
2024-02-21 01:41:11 +0400

    Added Urbanist Font

--- App/src/Layouts/MainLayout.jsx
+++ App/src/Layouts/MainLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import  Footer  from "../Pages/Footer";
-import { Header } from "../Pages/Header";
+import Footer from "../Footer/Footer";
+import { Header } from "../Header/Header";
+import Styles from "../Layouts/MainLayout.module.css"
 export function MainLayout() {
   return (
-    <div>
+    <div className={Styles.Main}>
       <Header/>
       <Outlet />
       <Footer />
