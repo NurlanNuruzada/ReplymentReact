e0db274f5422ddf242376e83680568c5814e2909
nurlannuruzada <nurlangn@code.edu.az>
2024-02-21 10:38:08 +0400

    Acardion done

--- App/src/Components/FrequentlyQuestions/Questions.jsx
+++ App/src/Components/FrequentlyQuestions/Questions.jsx
@@ -5,136 +5,92 @@ import Accordion, { accordionClasses } from '@mui/joy/Accordion';
 import AccordionDetails from '@mui/joy/AccordionDetails';
 import AccordionSummary from '@mui/joy/AccordionSummary';
 
-
 export default function Questions() {
     const [index, setIndex] = React.useState(0);
+
     return (
-        <>
-            <div className={Styles.FreQuestionsMain}>
-                <div className={Styles.FreQuestions}>
-                    <div className={Styles.FreQuestions_title}>
-                        <div>
-                            <p className={Styles.Title}>Frequently asked questions</p>
-                        </div>
+        <div className={Styles.FreQuestionsMain}>
+            <div className={Styles.FreQuestions}>
+                <div className={Styles.FreQuestions_title}>
+                    <div>
+                        <p className={Styles.Title}>Frequently asked questions</p>
                     </div>
-                    <div className={Styles.FreQuestions_question}>
-                        <div className={Styles.accordionWrapper}>
-                            <AccordionGroup
-                                sx={{
-                                    [`& .${accordionClasses.root}`]: {
-                                        padding: 2,
-                                        margin: "5px",
-                                        maxWidth: "100%",
-                                        border: '2px solid #F2F2F2',
-                                        borderRadius: "22px",
-                                        fontSize: "22px",
-                                        lineHeight: "110%",
+                </div>
+                <div className={Styles.FreQuestions_question}>
+                    <div className={Styles.accordionWrapper}>
+                        <AccordionGroup
+                            sx={{
+                                [`& .${accordionClasses.root}`]: {
+                                    padding: 2,
+                                    margin: "5px",
+                                    maxWidth: "100%",
+                                    border: '2px solid #F2F2F2',
+                                    borderRadius: "20px",
+                                    fontSize: "22px",
+                                    lineHeight: "110%",
+                                    transition: '0.2s ease',
+                                    '& button:not([aria-expanded="true"])': {
                                         transition: '0.2s ease',
-                                        '& button:not([aria-expanded="true"])': {
-                                            transition: '0.2s ease',
-                                        },
-                                        '& button:hover': {
-                                            background: 'transparent',
-                                        },
                                     },
-                                    [`& .${accordionClasses.root}.${accordionClasses.expanded}`]: {
-                                        bgcolor: 'none',
+                                    '& button:hover': {
+                                        background: 'transparent',
                                     },
-                                    '& [aria-expanded="true"]': {
-                                        boxShadow: (theme) => `inset 0 -1px 0 ${theme.vars.palette.divider}`,
+                                    '& .MuiAccordionSummary-expandIcon': {
+                                        width: '30px',
                                     },
-                                }}
-                            >
+                                    '& .MuiSvgIcon-root.MuiSvgIcon-sizeMd.css-1r2bn3-JoySvgIcon-root': {
+                                        fontSize: "40px",
+                                        color:"#616063"
+                                    },
+                                },
+                                [`& .${accordionClasses.root}.${accordionClasses.expanded}`]: {
+                                    bgcolor: 'none',
+                                },
+                                '& [aria-expanded="true"]': {
+                                    boxShadow: (theme) => `inset 0 -1px 0 ${theme.vars.palette.divider}`,
+                                },
+                            }}
+                        >
+                            {[...Array(5)].map((_, i) => (
                                 <Accordion
-                                    expanded={index === 0}
+                                    key={i}
+                                    expanded={index === i}
                                     onChange={(event, expanded) => {
-                                        setIndex(expanded ? 0 : null);
+                                        setIndex(expanded ? i : null);
+                                    }}
+                                    sx={{
+                                        padding: "0px 31px !important" // Same padding for both states
                                     }}
                                 >
-                                    <AccordionSummary><p style={{ fontWeight: 600 }}>Is replyment free?</p></AccordionSummary>
+                                    <AccordionSummary
+                                        sx={{
+                                            '&.MuiAccordionSummary-root': {
+                                                button: {
+                                                    padding: index === i ? '12px 8px' : '12px 8px'
+                                                }
+                                            }
+                                        }}
+                                    >
+                                        <p style={{ fontWeight: 600 }}>Accordion Item {i + 1}</p>
+                                    </AccordionSummary>
                                     <AccordionDetails
                                         sx={{
-                                            color: "616063",
+                                            color: "#616063",
                                             fontWeight: '500',
-                                            fontSize: "16px"
-                                        }}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua.
+                                            fontSize: "16px",
+                                            '&.MuiAccordionDetails-root': {
+                                                padding: index === i ? '20px 0': '0px'
+                                            }
+                                        }}
+                                    >
+                                        Content for Accordion Item {i + 1}
                                     </AccordionDetails>
                                 </Accordion>
-                                <Accordion
-                                    expanded={index === 1}
-                                    onChange={(event, expanded) => {
-                                        setIndex(expanded ? 1 : null);
-                                    }}
-                                >
-                                    <AccordionSummary><p style={{ fontWeight: 600 }}>Is replyment free?</p></AccordionSummary>
-                                    <AccordionDetails
-                                        sx={{
-                                            color: "616063",
-                                            fontWeight: '500',
-                                            fontSize: "16px"
-                                        }}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua.
-                                    </AccordionDetails>
-                                </Accordion>
-                                <Accordion
-                                    expanded={index === 1}
-                                    onChange={(event, expanded) => {
-                                        setIndex(expanded ? 1 : null);
-                                    }}
-                                >
-                                    <AccordionSummary><p style={{ fontWeight: 600 }}>Is replyment free?</p></AccordionSummary>
-                                    <AccordionDetails
-                                        sx={{
-                                            color: "616063",
-                                            fontWeight: '500',
-                                            fontSize: "16px"
-                                        }}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua.
-                                    </AccordionDetails>
-                                </Accordion>
-                                <Accordion
-                                    expanded={index === 1}
-                                    onChange={(event, expanded) => {
-                                        setIndex(expanded ? 1 : null);
-                                    }}
-                                >
-                                    <AccordionSummary><p style={{ fontWeight: 600 }}>Is replyment free?</p></AccordionSummary>
-                                    <AccordionDetails
-                                        sx={{
-                                            color: "616063",
-                                            fontWeight: '500',
-                                            fontSize: "16px"
-                                        }}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua.
-                                    </AccordionDetails>
-                                </Accordion>
-                                <Accordion
-                                    expanded={index === 1}
-                                    onChange={(event, expanded) => {
-                                        setIndex(expanded ? 1 : null);
-                                    }}
-                                >
-                                    <AccordionSummary><p style={{ fontWeight: 600 }}>Is replyment free?</p></AccordionSummary>
-                                    <AccordionDetails
-                                        sx={{
-                                            color: "616063",
-                                            fontWeight: '500',
-                                            fontSize: "16px"
-                                        }}>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua.
-                                    </AccordionDetails>
-                                </Accordion>
-                            </AccordionGroup>
-                        </div>
+                            ))}
+                        </AccordionGroup>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
