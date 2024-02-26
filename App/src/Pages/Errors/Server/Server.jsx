2a65a47ba48af47269498c7a2228a4d315e11a34
Kerimov056 <ulvisk@code.edu.az>
2024-02-27 01:53:07 +0400

    NotFound And Error Page Responsive

--- App/src/Pages/Errors/Server/Server.jsx
+++ App/src/Pages/Errors/Server/Server.jsx
@@ -3,18 +3,15 @@ import Styles from './Server.module.css';
 import Navbar from '../../../Navbar/Navbar';
 
 const Server = () => {
-    const { innerWidth: width, innerHeight: height } = window;
     return (
-        <div style={{ width: width, height: height }} className={Styles.NotFoundMain}>
+        <div className={Styles.NotFoundMain}>
             <Navbar />
-            <div className={Styles.dordSifirDordMain}>
-                <div className={Styles.dordSifirDord}>
-                    <div className={Styles.dordSifirDord_satusCode}>
-                        <h1>500</h1>
-                    </div>
-                    <div className={Styles.dordSifirDord_goBack}>
-                        <button>Go Back</button>
-                    </div>
+            <div className={Styles.content}>
+                <div className={Styles.statusCode}>
+                    <h1>500</h1>
+                </div>
+                <div className={Styles.goBack}>
+                    <button>Go Back</button>
                 </div>
             </div>
         </div>
