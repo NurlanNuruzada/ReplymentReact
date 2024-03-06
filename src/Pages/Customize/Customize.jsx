b4d1be1dd4b905b32e1c169bb5e2d8e113225b6f
nurlannuruzada <nurlangn@code.edu.az>
2024-03-06 04:19:41 +0400

    customize page added

--- src/Pages/Customize/Customize.jsx
+++ src/Pages/Customize/Customize.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Styles from './Customize.module.css'
+import PlusImage from '../../Images/add.svg'
 
 export default function Customize() {
   return (
-    <div className={Styles.Main}>Customize</div>
+    <div className={Styles.MainContainer}>
+      <div className={Styles.Header}>
+        <h1 className={Styles.Title}>Customize</h1>
+        <button className={Styles.AddDomainButton} ><img className={Styles.PlusIcon} src={PlusImage} alt="" />Add new domain</button>
+      </div>
+      <div className={Styles.ContentContainer}>
+      <button className={Styles.AddDomainButtonLarge}><img className={Styles.PlusIconLarge} src={PlusImage} alt="" />Add new domain</button>
+      </div>
+    </div>
   )
 }
